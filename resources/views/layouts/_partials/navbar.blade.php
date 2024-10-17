20f59176cb9a12b15339a4992036a6dd024ca040
BrantLauro <brantlauro@gmail.com>
2024-10-16 21:24:56 -0300

    [ADD] logo adicionado no header

--- resources/views/layouts/_partials/navbar.blade.php
+++ resources/views/layouts/_partials/navbar.blade.php
@@ -1,5 +1,7 @@
 <div>
-    <header class="d-flex justify-content-center py-3 bg-dark">
-
+    <header class="d-flex justify-content-center py-3 bg-dark">     
+        <a href="{{ route('home') }}" class="w-100 text-center">
+            <img src="{{ asset('images/svg/header-logo.svg') }}" alt="" width="20%">
+        </a>
     </header>
-  </div>
+</div>
